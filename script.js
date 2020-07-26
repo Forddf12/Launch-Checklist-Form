1ef275dac3b67487309af8d9f9b9c055d26a6bf6
Doug F <forddf12@outlook.com>
2020-07-26 17:42:07 -0500

    TurningIn

--- script.js
+++ script.js
@@ -21,6 +21,15 @@ window.addEventListener("load", function() {
                `
             })   //index selects random and data = json.name,
          })
+   // Function to check letters and numbers
+function alpha(name) {
+ let letters = /^[a-zA-Z]+$/;
+ if(name.value.match(letters)) {
+   return true;
+  } else { 
+   return false; 
+  }
+  }
    let form = document.querySelector("form");
    form.addEventListener("submit", function(event) {
       event.preventDefault();
@@ -28,23 +37,35 @@ window.addEventListener("load", function() {
       let copilotNameInput = document.querySelector("input[name=copilotName]");
       let fuelLevelInput = document.querySelector("input[name=fuelLevel]");
       let cargoMassInput = document.querySelector("input[name=cargoMass");
-         let faultyItems = document.getElementById("faultyItems");
+      let faultyItems = document.getElementById("faultyItems");
          
       if(pilotNameInput.value === "" || copilotNameInput.value === "" || fuelLevelInput.value === "" || cargoMassInput.value === "") {
-        alert("All fields are required!");         
-      } else if (isNaN(fuelLevelInput.value) === true || fuelLevelInput.value <= 0 || isNaN(cargoMassInput) === true || cargoMassInput.value <= 0) {
-         alert("Please insert a valid number for fuel!")
+        alert("All fields are required!");   
+        (faultyItems.style.visibility != "visible");             
+      } else if(alpha(pilotNameInput) === false) {  
+         alert("You must enter a valid name!")
+         (faultyItems.style.visibility != "visible");  
+      } else if(alpha(copilotNameInput) === false) {
+         alert("You must enter a valid name!")   
+         (faultyItems.style.visibility != "visible"); 
+      } else if (isNaN(fuelLevelInput.value)) {
+         alert("Please enter a valid number for fuel!");
+         (faultyItems.style.visibility != "visible");  
+      } else if(isNaN(cargoMassInput.value)) {
+         alert("Please enter a valid number for cargo mass!");   
+         (faultyItems.style.visibility != "visible");  
       } else 
          (faultyItems.style.visibility = "visible");
       
         
-         if(Number(fuelLevelInput.value) < 10000) {
+         if(fuelLevelInput.value < 10000) {
+            faultyItems.style.visibility = "visible";
             pilotStatus.innerHTML = `Pilot ${pilotNameInput.value} is ready for launch.`;
             copilotStatus.innerHTML = `Copilot ${copilotNameInput.value} is ready for launch.`;
             fuelStatus.innerHTML = "Fuel too low for launch";
             launchStatus.innerHTML = "Shuttle not ready for launch.";
             launchStatus.style.color = "red";
-         } else if(Number(cargoMassInput.value) > 10000) {
+         } else if(cargoMassInput.value > 10000) {
             faultyItems.style.visibility = "visible";
             pilotStatus.innerHTML = `Pilot ${pilotNameInput.value} is ready for launch.`;
             copilotStatus.innerHTML = `Copilot ${copilotNameInput.value} is ready for launch.`;
